61ce5b580bbef7f75020ff0ac04b422eb64ab29f
Soós Sándor <sasi-30@hotmail.com>
2020-06-20 22:09:34 +0100

    Szövegbox el helyezve!

--- WindowsFormsApplication1/WindowsFormsApplication1/Program.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Program.cs
@@ -14,8 +14,8 @@ namespace WindowsFormsApplication1
         [STAThread]
         static void Main()
         {
-            /// Application.EnableVisualStyles();
-            /// Application.SetCompatibleTextRenderingDefault(false);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
     }
